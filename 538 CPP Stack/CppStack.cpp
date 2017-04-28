8e3621b6da7f395eee8c38756c17f609d7052911
Pierre Violent (Ubuntu/Win Skull) <FrenchBear38@gmail.com>
2017-04-28 23:19:25 +0200

    Ubunto on Windows update

--- 538 CPP Stack/CppStack.cpp
+++ 538 CPP Stack/CppStack.cpp
@@ -1,7 +1,9 @@
 // CppStack.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <stack>
 
 
 using namespace std;
