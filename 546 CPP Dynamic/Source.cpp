f7dd64a1858be7c7987f2eaa349b406a3ae323e4
French Bear <FrenchBear38@outlook.com>
2017-04-29 10:08:47 +0200

    Code clean-up and comments in VS2017

--- 546 CPP Dynamic/Source.cpp
+++ 546 CPP Dynamic/Source.cpp
@@ -1,6 +1,8 @@
 // CPP Factorial
 // An implementation of factorial in C++ at compile-time based on template
-// 2017-04-09	PV	Added constexpr version, GitHub, Linux support
+// Also show type restictor to limit accepted types from a template
+//
+// 2017-04-09	PV	Added constexpr version, static_assert, GitHub, Linux support
 
 #include <stdio.h>
 #include <iostream>
@@ -8,6 +10,7 @@
 using namespace std;
 
 
+// template-based version
 template <int N> struct Factorial
 {
 	static const int value = N * Factorial<N - 1>::value; // recursive!
@@ -18,18 +21,34 @@ template <> struct Factorial<0> // required for terminating condition
 	static const int value = 1;
 };
 
+
+// constepr-based version
+constexpr long fact(long n) {
+	long res = 1;
+	while (n > 1)
+		res *= n--;
+	return res;
+}
+
 void TestFactorial()
 {
-	int x = Factorial<10>::value; // 3628800, known at compile time
-	cout << x << endl;
+	int x = Factorial<10>::value;	// 3628800, known at compile time
+	cout << "Factorial<10>::value = " << x << endl;
+	int y = fact(10);				// 3628800, also known at compile time
+	cout << "fact(10) = " << y << endl << endl;;
+
+	// Check that both are computed at compile-time
+	static_assert(fact(5) == Factorial<5>::value, "There is a problem");
 }
 
 
-
+// Example of template that only accepts float or double
+// From http://conradsanderson.id.au/misc/sanderson_templates_lecture_uqcomp7305.pdf
 template <typename T> struct restrictor { };
 template <> struct restrictor<float> { typedef float result; };
 template <> struct restrictor<double> { typedef double result; };
 
+// Can only compute distance on float and double types 
 template <typename T> typename restrictor<T>::result distance(T a1, T a2, T b1, T b2) {
 	T tmp1 = a1 - b1;
 	T tmp2 = a2 - b2;
@@ -43,6 +62,7 @@ int main() {
 	double x1 = 0.0, y1 = 0.0, x2 = 4.0, y2 = 3.0;
 	double d = distance<double>(x1, y1, x2, y2);
 	cout << d << endl;
+
 
 #ifdef _WIN32
 	cout << "\n(Pause) ";
