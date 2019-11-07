eb9c432bfac6af801d11f3679efe882a676ef0cd
French Bear <FrenchBear38@outlook.com>
2019-11-07 23:24:36 +0100

    Unicode and Utf8 examples

---
+++ 700 cpputf8/cpputf8.cpp
@@ -0,0 +1,26 @@
+// cpputf8: test of utf8 in C++
+// From http://utfcpp.sourceforge.net/
+// 2019-11-07   PV
+
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+#include "utf8.h"
+
+using namespace std;
+using namespace utf8;
+
+int main()
+{
+    // Convert utf8 to utf32
+    char *u8str = "Aé♫山𝄞🐗";      // Do not use u8 prefix!!!  This source is already utf-8 encoded
+    vector<int> utf32result;
+    unchecked::utf8to32(u8str, u8str + strlen(u8str), back_inserter(utf32result));
+    assert(utf32result.size() == 6);
+
+    // And convert back to utf8
+    vector<unsigned char> utf8result;
+    utf32to8(begin(utf32result), end(utf32result), back_inserter(utf8result));
+    assert(strlen(u8str) == utf8result.size());
+}
---
+++ 701 cppunicode/GenerateCppUnicodeData/GenerateCppUnicodeData/GenerateCppUnicodeData.cs
@@ -0,0 +1,40 @@
+﻿// Generates a table of codepoint -> codepoint lowercase no accent for C++
+// Only for chars in following blocks:
+// 0020..007F;Basic Latin(ASCII); Latin;European Scripts; Scripts
+// 0080..009F;ASCII Controls C1;;Specials;Symbols and Punctuation
+// 00A0..00FF;Latin-1 Supplement;Latin;European Scripts; Scripts
+// 0100..017F;Latin Extended-A;Latin;European Scripts; Scripts
+// 0180..024F;Latin Extended-B;Latin;European Scripts; Scripts
+//
+// 2019-11-07   PV
+
+using System;
+using System.Diagnostics;
+using System.Text;
+using static System.Console;
+
+namespace GenerateCppUnicodeData
+{
+    class GenerateCppUnicodeData
+    {
+        static void Main()
+        {
+            OutputEncoding = System.Text.Encoding.UTF8;
+            for (int i = 32; i < 0x24f; i++)
+            {
+                string s = new string((char)i, 1);
+                string t = s.Normalize(NormalizationForm.FormD).ToLowerInvariant();
+                string u = "";
+                foreach (char c in t)
+                    if (char.IsLetter(c))
+                        u += new string(c, 1);
+
+                if (s != u && u.Length>=1 && (int)u[0]<=0x24f)
+                {
+                    if (u.Length != 1) Debugger.Break();
+                    WriteLine($"{{0x{i:x}, 0x{(int)u[0]:x}}},\t// {(char)i}");
+                }
+            }
+        }
+    }
+}
