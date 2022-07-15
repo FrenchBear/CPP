46dffbdb516675a94d0450a71518dd740aaa1ad1
French Bear <FrenchBear38@outlook.com>
2022-07-15 11:22:37 +0200

    save 802_Init

--- 801_Tuple/build.bat
+++ 801_Tuple/build.bat
@@ -1,2 +1,3 @@
 @if "%VCToolsVersion%"=="" call devcmd
+echo cl /W4 /Zi /EHsc /std:c++20 cpptuple.cpp
 cl /W4 /Zi /EHsc /std:c++20 cpptuple.cpp
---
+++ 802_Init/init.cpp
@@ -0,0 +1,20 @@
+﻿// CS802_Init.cpp
+// Play with constructors and initialization in C++
+//
+// 2022-07-15	PV		First version
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << "C++ init" << endl;
+
+
+	return 0;
+}
